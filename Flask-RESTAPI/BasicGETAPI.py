8734f280c95f1505f53d1258949425de83adb89a
SohamGlobal <sohamglobal@users.noreply.github.com>
2025-10-15 19:39:38 +0530

    Add files via upload

---
+++ Flask-RESTAPI/BasicGETAPI.py
@@ -0,0 +1,26 @@
+# pip install flask_restful
+
+from flask import Flask
+from flask_restful import Resource,Api
+
+app=Flask(__name__)
+api=Api(app)
+
+class BasicREST(Resource):
+    def get(self):
+        profile={
+            "name":"praffull",
+            "codename":"ethan hunt",
+            "city":"london",
+            "language":"english",
+            "qualification":"MBA(MC)",
+            "mobile":"7391966656",
+            "email":"praffull@outlook.com",
+            "keyskills":["java","spring boot","jpa","ai"]
+        }
+        return profile
+
+api.add_resource(BasicREST,"/profile")
+app.run(debug=True)
+
+
